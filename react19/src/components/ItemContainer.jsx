cfdfc8e61d561f3f17c0701f728aa56f587674e6
Mohammad Shahid <msa.ansari28@gmail.com>
2025-08-12 12:41:43 +0530

    mapDispatchToProps

--- react19/src/components/ItemContainer.jsx
+++ react19/src/components/ItemContainer.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import { buyCake, buyIceCream } from '../redux'
 
 function ItemContainer(props) {
+  console.log(props)
   return (
     <div>
-        <h2> Item - {props.item}</h2>
+        <h2 className='text-2xl font-bold text-center text-gray-800 mb-4'> Item - {props.item}</h2>
+        <button 
+        onClick={props.buyItem}
+        className='w-32 font-semibold py-2 px-4 rounded-lg transition bg-blue-600 hover:bg-blue-700 text-white'
+        >Buy Item</button>
     </div>
   )
 }
@@ -19,4 +25,17 @@ const mapStateToProps = (state, ownProps) => {
     }
 }
 
-export default connect(mapStateToProps)(ItemContainer)
+const mapDispatchToProps = (dispatch, ownProps) => {
+  const dispatchFunction = ownProps.cake 
+                          ? () => dispatch(buyCake())
+                          : () => dispatch(buyIceCream())
+
+      return{
+        buyItem: dispatchFunction
+      }
+}
+
+export default connect(
+                        mapStateToProps, 
+                        mapDispatchToProps
+                      )(ItemContainer)
