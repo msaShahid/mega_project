1449d7296288a0b60d31766c8bab0ad6b2f50fbe
Mohammad Shahid <msa.ansari28@gmail.com>
2025-08-28 15:31:16 +0530

    `feat: implement error middleware`

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import connectDB from './config/db.js';
+import errorHandler from './middleware/error.middleware.js';
 
 
 dotenv.config();
@@ -11,6 +12,8 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use(errorHandler)
 
 
 const PORT = process.env.PORT || 5000;
