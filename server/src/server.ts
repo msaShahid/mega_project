79834a6464baa36bfc0b501eb60ceefbd3fb8fd5
Mohammad Shahid <msa.ansari28@gmail.com>
2025-09-23 11:39:19 +0530

    `refactor(server.ts): integrate socket.io and clean up route imports using aliases`

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,13 +3,19 @@ import cors from 'cors';
 import morgan from 'morgan';
 import compression from 'compression';
 import helmet from 'helmet';
+import { Server } from 'socket.io';
+import http from 'http';
 
-import config from './config/index'; 
-import connectDB from './config/db';
-import logger from './utils/logger';
+import config from '@config/index'; 
+import connectDB from '@config/db';
+import logger from '@utils/logger';
 import errorHandler from './middlewares/error.middleware';
-import limiter from './middlewares/rateLimiter'
-import authRoutes from './routes/auth.routes';
+import limiter from '@middlewares/rateLimiter'
+import authRoutes from '@routes/auth.routes';
+import chatRoute from  '@routes/chat.routes'
+import friendRoute from  '@routes/friend.routes'
+import { authenticateSocket } from 'socket/authenticateSocket';
+import { registerChatHandlers } from 'socket/chatHandlers';
 
 // Connect to MongoDB
 connectDB(config.mongoURI);
@@ -36,7 +42,29 @@ if (process.env.NODE_ENV !== 'test') {
   );
 }
 
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: { origin: '*' }, // adjust as needed
+});
+
+// Use socket authentication
+io.use(authenticateSocket);
+
+// Handle authenticated socket connections
+io.on('connection', (socket) => {
+  const user = socket.data.user;
+  console.log('Socket connected:', user?.email);
+
+  registerChatHandlers(io, socket); // Chat logic here
+
+  socket.on('disconnect', () => {
+    console.log('Socket disconnected:', user?.email);
+  });
+})
+
 app.use('/api/auth', authRoutes);
+app.use('/api/chat', chatRoute);
+app.use('/api/friends', friendRoute);
 
 app.get('/', (req, res) => {
   res.send('Hello Secure TypeScript World!');
