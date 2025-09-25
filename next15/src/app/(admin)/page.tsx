4218fe11b576f7f4026d28158352f0d412416abc
Mohammad Shahid <msa.ansari28@gmail.com>
2025-09-25 22:29:44 +0530

    `update page`

--- next15/src/app/(admin)/page.tsx
+++ next15/src/app/(admin)/page.tsx
@@ -10,7 +10,7 @@ export const metadata: Metadata = {
   description: " Home for ",
 };
 
-export default function Ecommerce() {
+export default function Home() {
   return (
     <div className="grid grid-cols-12 gap-4 md:gap-6">
       <div className="col-span-12 space-y-6 xl:col-span-7">
